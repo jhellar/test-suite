704ca70473f180edd2f0e83cfef15987e5fc9f65
Jan Hellar <jhellar@redhat.com>
2019-09-12 15:40:38 +0200

    Bash -> TS for prepare script

---
+++ scripts/prepare.ts
@@ -0,0 +1,65 @@
+import * as Api from 'kubernetes-client';
+import * as Mustache from 'mustache';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const mobileClientCRD = require('../crds/mobile-client-crd');
+ 
+const Client = Api.Client1_13;
+const config = Api.config;
+const client = new Client({ config: config.fromKubeconfig(), version: '1.11' });
+
+client.addCustomResourceDefinition(mobileClientCRD);
+
+const mobileClientCRTemplate = fs.readFileSync(
+  path.resolve(__dirname, '../templates/mobile-client.json'),
+  'utf8'
+);
+
+const appNamePrefix = 'test-';
+const appName = `${appNamePrefix}${Math.random().toString(36).substring(7)}`;
+
+const mobileClientCR = Mustache.render(mobileClientCRTemplate, { appName });
+
+(async function() {
+  let mobileClient;
+  let mdcNamespace;
+  const namespaces = [
+    'mobile-developer-console',
+    'openshift-mobile-developer-console'
+  ];
+  while (true) {
+    try {
+      const mdcNamespace = namespaces.pop();
+      mobileClient = await client
+        .apis['mdc.aerogear.org']
+        .v1alpha1
+        .namespaces(mdcNamespace)
+        .mobileclients
+        .post({ body: JSON.parse(mobileClientCR) });
+      break;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const mobileClients = await client
+    .apis['mdc.aerogear.org']
+    .v1alpha1
+    .namespaces(mdcNamespace)
+    .mobileclients
+    .get();
+
+  const appsToDelete = mobileClients.body.items
+    .map(i => i.spec.name)
+    .filter(name => name !== appName && name.startsWith(appNamePrefix));
+
+  for (const app of appsToDelete) {
+    await client
+      .apis['mdc.aerogear.org']
+      .v1alpha1
+      .namespaces(mdcNamespace)
+      .mobileclients(app)
+      .delete();
+  }
+})();
